5a896566c519d086789ae03bee9fd2c7318cc488
Wrightl71559 <60583421+Wrightl71559@users.noreply.github.com>
2020-06-10 13:04:28 +1200

    componet1a written

---
+++ 01_getinput.py
@@ -0,0 +1,28 @@
+# component 1 a - get input
+
+# put in string checking function from miss gottschalk
+# set up question to ask user
+# return user input
+
+
+# string checking function (made by Miss Gottschalk)
+def string_checker(question, to_check):
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for item in to_check:
+            if response == item:
+                return response
+            elif response == item[0]:
+                return item
+
+        print("sorry that is not a valid response")
+
+# Main Routine
+
+q_options = ["a", "b"]
+
+q_type = string_checker("for square root questions enter <a> or for squaring questions enter <b>", q_options)
+print(q_type)
---
+++ 01_getinput_b.py
@@ -0,0 +1,6 @@
+# component 1 b - get input
+
+# put in integer checking function from previous code
+# set up question to ask user
+# return user input
+
