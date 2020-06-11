3f2f1e15afbed0c9b295831c2df54471d8803c47
Wrightl71559 <60583421+Wrightl71559@users.noreply.github.com>
2020-06-11 12:05:52 +1200

    bothpart of component 1 done

--- 01_getinput_b.py
+++ 01_getinput_b.py
@@ -6,4 +6,22 @@
 
 
 # integer checking function
+def intcheck(question, low, high):
+    valid = False
+    error = ("Please enter an integer between 1 and 10 (inclusive)")
+    while not valid:
+       try:
+            response = int(input(question))
 
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+                print()
+       except ValueError:
+           print(error)
+
+
+# Main Routine
+q_number = intcheck("How many questions would you like? (up to 10)  ", 1, 10)
+print(q_number)
